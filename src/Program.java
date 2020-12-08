eb509a69cbc5a79e98816e7742a099a482b54e91
danielvasic <dvasic@gmail.com>
2020-12-08 12:34:38 +0100

    Vježbe 08.12.2020.

--- src/Program.java
+++ src/Program.java
@@ -1,18 +1,34 @@
-import model.Baza;
 import model.Korisnik;
+import model.Nastavnik;
 import model.Tablica;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public class Program {
     public static void main (String [] args){
-
+        /*
+        Korisnik k = new Korisnik(
+                "peroperic",
+                "123456",
+                "Administrator"
+        );
         try {
-            List<Korisnik> korisnici = (List<Korisnik>) Tablica.dohvatiSve(Korisnik.class);
-            System.out.println(korisnici);
+            k.spasi();
+            Nastavnik n = new Nastavnik(
+                    "Pero",
+                    "Perić",
+                    "1234567",
+                    k.getID()
+            );
+
+            n.spasi();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+         */
+        try {
+            Korisnik pero = (Korisnik) Tablica.dohvati(Korisnik.class, 9);
+            pero.Uloga = "Nastavnik";
+            pero.uredi();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- src/model/Korisnik.java
+++ src/model/Korisnik.java
@@ -1,8 +1,19 @@
 package model;
 
-public class Korisnik {
-    Long ID;
-    String KorisnickoIme;
-    String Lozinka;
-    String Uloga;
+public class Korisnik extends Tablica {
+    public Korisnik() {}
+    public Korisnik(String korisnickoIme, String lozinka, String uloga) {
+        KorisnickoIme = korisnickoIme;
+        Lozinka = lozinka;
+        Uloga = uloga;
+    }
+
+    public Long ID;
+    public String KorisnickoIme;
+    public String Lozinka;
+    public String Uloga;
+
+    public Long getID() {
+        return ID;
+    }
 }
