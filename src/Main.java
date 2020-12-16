2bc600ec462c3e22093d87599216b4db8aea460c
danielvasic <dvasic@gmail.com>
2020-12-16 11:54:17 +0100

    Dodatna konfiguracija - Vježbe 15.12.2020.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/LoginView.fxml"));
@@ -13,7 +12,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
